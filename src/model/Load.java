9d07713896ed68165cfe2772a160ca603332e517
Juan Fernando Martinez Dominguez <jm1811324@gmail.com>
2020-04-06 12:49:50 -0500

    a commit was created in the class Load

--- src/model/Load.java
+++ src/model/Load.java
@@ -55,7 +55,14 @@ public class Load {
 	public void setType(int value){
 		type = value;
 	}
+	/*
+	getTotalWeight
+	<b>description: </b> this method will calculate the total weight based on the box Quantity and the weight Per Box <br/>
+	<b>pre:</b> the class must be create and the attribs boxes and weightPerBox must be instantiated <br/>
+	<b>post:</b> return a double which is the total weight of the load <br/>
+	@params: <br/>
 	
+	*/
 	public double getTotalWeight(){
 		totalWeight = boxes*weightPerBox;
 		return totalWeight;
